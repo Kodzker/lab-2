cfa4901e1db08e5be7fff88995cec4284401e02d
Kodzker <72316569+Kodzker@users.noreply.github.com>
2021-03-31 13:42:49 +0300

    Update library.py

--- library.py
+++ library.py
@@ -100,7 +100,7 @@ def sort_students(array: list) -> list:
         # Sort by bubble method:
         for i in range(length):
             if i + 1 < length and average_score(array[i]) >
-            average_score(array[i + 1]):
+                    average_score(array[i + 1]):
                 array[i], array[i + 1] = array[i + 1], array[i]
 
                 # I will say that the array is still sorted:
