fcddf25715374d5a96c65d2825c97a6aba4ce369
Kodzker <72316569+Kodzker@users.noreply.github.com>
2021-03-31 16:54:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,40 +1,22 @@
 # translation from google.
-
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print('Hi,', name)  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-
-if __name__ == '__main__':
-    print_hi('PyCharm')
-else:
-    exit()
-
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# The code was written by Timofey Bochkor, Einstein and others...
+
+if __name__ != '__main__':
+    print("This is a program, not a library!")
+    exit()
 
 from library import *
 
 print('')
 
 array = get_students()  # получить массив студентов.
-
-# echo_students(array)
-
+#echo_students(array)
 print('')
 
 array = sort_students(array)  # сортировка массива студентов
-
 echo_students(array)
-
-print()
+print('')
 
 print('best:')
 echo_best_students(array)  # вывод учиников у которых есть 4 и 5.
